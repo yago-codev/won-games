9e277205c65a4fdc5942921fd6384941f7a84925
yago-codev <yagomilano92@gmail.com>
2023-06-08 16:41:40 -0300

    feat: aplicando propriedade para lidar com responvidade e testando a implementação

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -30,4 +30,14 @@ describe('<Logo />', () => {
       width: '20rem'
     })
   })
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo size="large" hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
+  })
 })
