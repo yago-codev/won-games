b9e9a530c7faa5e5e4cc51f0458283f6aab68e27
yago-codev <yagomilano92@gmail.com>
2023-06-08 15:10:01 -0300

    feat: criando testes para verificar a cor do <Logo />

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    // getByLabelText = aria-label
+    // parentElement vai acessar o elemento pai (Wrapper)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
