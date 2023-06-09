53dc7310468348655ec7aa74dca2e674fd8c205f
yago-codev <yagomilano92@gmail.com>
2023-06-09 20:42:05 -0300

    feat: criando propriedade fullWidth

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled, { DefaultTheme, css } from 'styled-components'
 
 import { ButtonProps } from '.'
 
-type WrapperProps = Pick<ButtonProps, 'size'>
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -11,23 +11,27 @@ const wrapperModifiers = {
   `,
   medium: (theme: DefaultTheme) => css`
     height: 4rem;
-    font-size: ${theme.font.sizes.medium};
+    font-size: ${theme.font.sizes.small};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
   `,
   large: (theme: DefaultTheme) => css`
     height: 5rem;
     font-size: ${theme.font.sizes.xlarge};
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+  fullWidth: () => css`
+    width: 100%;
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size }) => css`
+  ${({ theme, size, fullWidth }) => css`
     background: linear-gradient(180deg, '#FF5F5F' 0%, '#F062C0' 50%);
     color: ${theme.colors.white};
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
     ${!!size && wrapperModifiers[size](theme)};
+    ${fullWidth && wrapperModifiers.fullWidth()};
   `}
 `
